c46414de10e71d7359c5e411b9091bb82384c4b0
Phil Kirchner <phil.kirchner.999@googlemail.com>
2026-07-07 14:53:28 +0200

    NAV-Slider
    Im eingeklappten Zustand zeigt der NAV-Slider wirklich nur den NAV-Tab. Die Einträge 01 bis 05 sind dort komplett unsichtbar und nicht interaktiv.
    Im ausgeklappten Zustand bleiben 01 bis 05 dauerhaft leicht als Buttons erkennbar, auch ohne Hover.
    Build/Cache ist auf 20260707-pwaapp10 aktualisiert.

--- app.js
+++ app.js
@@ -3676,8 +3676,8 @@
 
   function setupHeroAvatarEgg() {
     const avatarSources = {
-      src: "./image/iconic-avatar-960.jpg?v=20260707-pwaapp8",
-      srcset: "./image/iconic-avatar-720.jpg?v=20260707-pwaapp8 720w, ./image/iconic-avatar-960.jpg?v=20260707-pwaapp8 960w",
+      src: "./image/iconic-avatar-960.jpg?v=20260707-pwaapp10",
+      srcset: "./image/iconic-avatar-720.jpg?v=20260707-pwaapp10 720w, ./image/iconic-avatar-960.jpg?v=20260707-pwaapp10 960w",
       alt: "Stilisiertes Hero-Portrait mit Iconic Avatar"
     };
 
